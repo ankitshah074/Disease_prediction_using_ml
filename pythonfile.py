b3dc1e12ff36f9406de2667d1c806b7907f4d0a5
ankitshah074 <122113701+ankitshah074@users.noreply.github.com>
2024-04-16 01:38:16 +0530

    Update pythonfile.py

--- pythonfile.py
+++ pythonfile.py
@@ -9,9 +9,9 @@ import pickle
 import streamlit as st
 from streamlit_option_menu import option_menu
 
-diabetes_model = pickle.load(open('C:/Users/ankit/Downloads/project/models/diabetes_model.sav', 'rb'))
+diabetes_model = pickle.load(open('diabetes_model.sav', 'rb'))
 
-heart_disease_model = pickle.load(open('C:/Users/ankit/Downloads/project/models/heart_disease_model.sav', 'rb'))
+heart_disease_model = pickle.load(open('heart_disease_model.sav', 'rb'))
 
 
 with st.sidebar:
